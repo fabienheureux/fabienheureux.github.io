4342b48b6cd352629b12bbf63370d21961b51e4e
Fabien Le Frapper <contact@fabienlefrapper.me>
2023-04-08 20:53:58 +0200

    Update console.logs

--- static/i-promise-it-is-only-a-few-lines.js
+++ static/i-promise-it-is-only-a-few-lines.js
@@ -6,12 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  console.log(`☞ Welcome to the browser console fellow traveller. \n\n`);
-
-  console.log(`\n\n
-	You might have opened it in order to check whether I track you. \n
-	I have a Plausible instance, that I ran on my own server. It is publicly available here: https://stats.fabien.cool/fabien.cool  \n
+  console.log(`\n\n\n☞ Welcome to the browser console fellow traveller. \n\n`);
+  console.log(`\n\nYou might have opened it in order to check if I track you. \n
+	I have a Plausible instance, that I ran on my own server. It is publicly available here: https://stats.fabien.cool/fabien.cool \n
 	I do not track any activity other than anonymous page views, that do not require the user's
 	consent as there are no personnal information stored or transmitted through these. 
 	`);
+  console.log(`\n\nIf you just want to take a look at the source code, it is available in
+	various locations : https://git.fabien.cool/fabienheureux/fabienheureux.github.io or 
+	http://github.com/fabienheureux/fabienheureux.github.io`);
 });
